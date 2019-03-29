86a1491569941ec76db95a342c5c4d49a08c7e2b
Vo Huu Ly <vhlee1996@gmail.com>
2019-03-29 23:04:49 +0700

    Add attributes

--- app/src/main/java/nguyendoannhan/com/content/nhan.java
+++ app/src/main/java/nguyendoannhan/com/content/nhan.java
@@ -1,4 +1,17 @@
 package nguyendoannhan.com.content;
 
 public class nhan {
+    private String mName;
+
+    public nhan(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
 }
